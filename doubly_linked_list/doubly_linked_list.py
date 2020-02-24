edb59b4c944bc4561d365aee4e9c3d00a078a9aa
Brennon Nuckols <brennuck@gmail.com>
2020-02-24 11:32:30 -0700

    delete

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -101,7 +101,24 @@ class DoublyLinkedList:
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        self.length -= 1
+
+        # If LL is empty
+        if not self.head and not self.tail:
+            # Error handling
+            return
+        # If head and tail
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+
+        elif self.head == node:
+            self.head = self.head.next
+            node.delete()
+
+        elif self.tail == node:
+            self.tail = self.tail.prev
+            node.delete()
         
     """Returns the highest value currently in the list"""
     def get_max(self):
